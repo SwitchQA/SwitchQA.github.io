8410f5b83df65f1e149bbffffc53db45853296b4
ISEP_Switch_QA_Luis <126819903+SwitchQA@users.noreply.github.com>
2023-11-17 18:21:50 +0000

    update

--- pickledFeatures.js
+++ pickledFeatures.js
@@ -1,6 +1,230 @@
 jsonPWrapper ({
   "Features": [
     {
+      "RelativeFolder": "delete.feature",
+      "Feature": {
+        "Name": "US 013 - Delete an account",
+        "Description": "",
+        "FeatureElements": [
+          {
+            "Name": "Successful account deletion",
+            "Slug": "successful-account-deletion",
+            "Description": "",
+            "Steps": [
+              {
+                "Keyword": "Given",
+                "NativeKeyword": "Given ",
+                "Name": "I am logged in as a member without any posts or comments",
+                "StepComments": [],
+                "AfterLastStepComments": []
+              },
+              {
+                "Keyword": "When",
+                "NativeKeyword": "When ",
+                "Name": "I click on the member page button and am redirected to the member page one",
+                "StepComments": [],
+                "AfterLastStepComments": []
+              },
+              {
+                "Keyword": "Then",
+                "NativeKeyword": "Then ",
+                "Name": "I click on the delete account button to delete member one",
+                "StepComments": [],
+                "AfterLastStepComments": []
+              },
+              {
+                "Keyword": "Then",
+                "NativeKeyword": "Then ",
+                "Name": "I should see a confirmation message \"User deleted successfully! 🎉\"",
+                "StepComments": [],
+                "AfterLastStepComments": []
+              }
+            ],
+            "Tags": [],
+            "Result": {
+              "WasExecuted": false,
+              "WasSuccessful": false,
+              "WasProvided": false
+            }
+          },
+          {
+            "Name": "Unsuccessful account deletion due to existing posts or comments",
+            "Slug": "unsuccessful-account-deletion-due-to-existing-posts-or-comments",
+            "Description": "",
+            "Steps": [
+              {
+                "Keyword": "Given",
+                "NativeKeyword": "Given ",
+                "Name": "I am logged in as a member which makes a post",
+                "StepComments": [],
+                "AfterLastStepComments": []
+              },
+              {
+                "Keyword": "When",
+                "NativeKeyword": "When ",
+                "Name": "I click on the member page button and am redirected to the member page two",
+                "StepComments": [],
+                "AfterLastStepComments": []
+              },
+              {
+                "Keyword": "Then",
+                "NativeKeyword": "Then ",
+                "Name": "I click on the delete account button to delete member two",
+                "StepComments": [],
+                "AfterLastStepComments": []
+              },
+              {
+                "Keyword": "Then",
+                "NativeKeyword": "Then ",
+                "Name": "I should see an error message \"Oops! Couldn't delete user. 🙁\"",
+                "StepComments": [],
+                "AfterLastStepComments": []
+              }
+            ],
+            "Tags": [],
+            "Result": {
+              "WasExecuted": false,
+              "WasSuccessful": false,
+              "WasProvided": false
+            }
+          }
+        ],
+        "Result": {
+          "WasExecuted": false,
+          "WasSuccessful": false,
+          "WasProvided": false
+        },
+        "Tags": []
+      },
+      "Result": {
+        "WasExecuted": false,
+        "WasSuccessful": false,
+        "WasProvided": false
+      }
+    },
+    {
+      "RelativeFolder": "topThreeMembers.feature",
+      "Feature": {
+        "Name": "US 018 - View top 3 Commenting Members for a Specific Day",
+        "Description": "",
+        "FeatureElements": [
+          {
+            "Name": "Get the top three commenting members for a specific day",
+            "Slug": "get-the-top-three-commenting-members-for-a-specific-day",
+            "Description": "",
+            "Steps": [
+              {
+                "Keyword": "Given",
+                "NativeKeyword": "Given ",
+                "Name": "I'm a logged in member in the statistics page",
+                "StepComments": [],
+                "AfterLastStepComments": []
+              },
+              {
+                "Keyword": "When",
+                "NativeKeyword": "When ",
+                "Name": "I enter the statistics page, type the date \"2023-11-13\" and click submit for box 3",
+                "StepComments": [],
+                "AfterLastStepComments": []
+              },
+              {
+                "Keyword": "Then",
+                "NativeKeyword": "Then ",
+                "Name": "The result for box 3 should be \"Result: RitaCastro, NatalyLucas, PauloTeixeira\"",
+                "StepComments": [],
+                "AfterLastStepComments": []
+              }
+            ],
+            "Tags": [],
+            "Result": {
+              "WasExecuted": false,
+              "WasSuccessful": false,
+              "WasProvided": false
+            }
+          },
+          {
+            "Name": "Get the top three commenting members for a specific day without a top three",
+            "Slug": "get-the-top-three-commenting-members-for-a-specific-day-without-a-top-three",
+            "Description": "",
+            "Steps": [
+              {
+                "Keyword": "Given",
+                "NativeKeyword": "Given ",
+                "Name": "I'm a logged in member in the statistics page in scenario 2",
+                "StepComments": [],
+                "AfterLastStepComments": []
+              },
+              {
+                "Keyword": "When",
+                "NativeKeyword": "When ",
+                "Name": "I enter the statistics page, type the date \"2023-11-10\" and click submit for box 3 in scenario 2",
+                "StepComments": [],
+                "AfterLastStepComments": []
+              },
+              {
+                "Keyword": "Then",
+                "NativeKeyword": "Then ",
+                "Name": "The result for two for box 3 should be \"Result: There isn't a top three for this date\" in scenario 2",
+                "StepComments": [],
+                "AfterLastStepComments": []
+              }
+            ],
+            "Tags": [],
+            "Result": {
+              "WasExecuted": false,
+              "WasSuccessful": false,
+              "WasProvided": false
+            }
+          },
+          {
+            "Name": "Get the top three commenting members for an invalid date",
+            "Slug": "get-the-top-three-commenting-members-for-an-invalid-date",
+            "Description": "",
+            "Steps": [
+              {
+                "Keyword": "Given",
+                "NativeKeyword": "Given ",
+                "Name": "I'm a logged in member in the statistics page in scenario 3",
+                "StepComments": [],
+                "AfterLastStepComments": []
+              },
+              {
+                "Keyword": "When",
+                "NativeKeyword": "When ",
+                "Name": "I enter an invalid date \"222\" and click submit for box 3 in scenario 3",
+                "StepComments": [],
+                "AfterLastStepComments": []
+              },
+              {
+                "Keyword": "Then",
+                "NativeKeyword": "Then ",
+                "Name": "The message \"Inserted date is not valid, please refresh and try again 😎\" should be displayed",
+                "StepComments": [],
+                "AfterLastStepComments": []
+              }
+            ],
+            "Tags": [],
+            "Result": {
+              "WasExecuted": false,
+              "WasSuccessful": false,
+              "WasProvided": false
+            }
+          }
+        ],
+        "Result": {
+          "WasExecuted": false,
+          "WasSuccessful": false,
+          "WasProvided": false
+        },
+        "Tags": []
+      },
+      "Result": {
+        "WasExecuted": false,
+        "WasSuccessful": false,
+        "WasProvided": false
+      }
+    },
+    {
       "RelativeFolder": "percentagePostsWithoutComments.feature",
       "Feature": {
         "Name": "US 020 - View Percentage of Posts Without Comments",
@@ -155,138 +379,50 @@ jsonPWrapper ({
         "WasSuccessful": false,
         "WasProvided": false
       }
-    },
-    {
-      "RelativeFolder": "delete.feature",
-      "Feature": {
-        "Name": "US 13 - Delete an account",
-        "Description": "",
-        "FeatureElements": [
-          {
-            "Name": "Successful account deletion",
-            "Slug": "successful-account-deletion",
-            "Description": "",
-            "Steps": [
-              {
-                "Keyword": "Given",
-                "NativeKeyword": "Given ",
-                "Name": "I am logged in as a member without any posts or comments",
-                "StepComments": [],
-                "AfterLastStepComments": []
-              },
-              {
-                "Keyword": "When",
-                "NativeKeyword": "When ",
-                "Name": "I click on the member page button and am redirected to the member page one",
-                "StepComments": [],
-                "AfterLastStepComments": []
-              },
-              {
-                "Keyword": "Then",
-                "NativeKeyword": "Then ",
-                "Name": "I click on the delete account button to delete member one",
-                "StepComments": [],
-                "AfterLastStepComments": []
-              },
-              {
-                "Keyword": "Then",
-                "NativeKeyword": "Then ",
-                "Name": "I should see a confirmation message \"User deleted successfully! 🎉\"",
-                "StepComments": [],
-                "AfterLastStepComments": []
-              }
-            ],
-            "Tags": [],
-            "Result": {
-              "WasExecuted": false,
-              "WasSuccessful": false,
-              "WasProvided": false
-            }
-          },
-          {
-            "Name": "Unsuccessful account deletion due to existing posts or comments",
-            "Slug": "unsuccessful-account-deletion-due-to-existing-posts-or-comments",
-            "Description": "",
-            "Steps": [
-              {
-                "Keyword": "Given",
-                "NativeKeyword": "Given ",
-                "Name": "I am logged in as a member which makes a post",
-                "StepComments": [],
-                "AfterLastStepComments": []
-              },
-              {
-                "Keyword": "When",
-                "NativeKeyword": "When ",
-                "Name": "I click on the member page button and am redirected to the member page two",
-                "StepComments": [],
-                "AfterLastStepComments": []
-              },
-              {
-                "Keyword": "Then",
-                "NativeKeyword": "Then ",
-                "Name": "I click on the delete account button to delete member two",
-                "StepComments": [],
-                "AfterLastStepComments": []
-              },
-              {
-                "Keyword": "Then",
-                "NativeKeyword": "Then ",
-                "Name": "I should see an error message \"Oops! Couldn't delete user. 🙁\"",
-                "StepComments": [],
-                "AfterLastStepComments": []
-              }
-            ],
-            "Tags": [],
-            "Result": {
-              "WasExecuted": false,
-              "WasSuccessful": false,
-              "WasProvided": false
-            }
-          }
-        ],
-        "Result": {
-          "WasExecuted": false,
-          "WasSuccessful": false,
-          "WasProvided": false
-        },
-        "Tags": []
-      },
-      "Result": {
-        "WasExecuted": false,
-        "WasSuccessful": false,
-        "WasProvided": false
-      }
     }
   ],
   "Summary": {
     "Tags": [],
     "Folders": [
       {
-        "Folder": "percentagePostsWithoutComments.feature",
-        "Total": 4,
-        "Passing": 0,
-        "Failing": 0,
-        "Inconclusive": 4
-      },
-      {
         "Folder": "delete.feature",
         "Total": 2,
         "Passing": 0,
         "Failing": 0,
         "Inconclusive": 2
+      },
+      {
+        "Folder": "topThreeMembers.feature",
+        "Total": 3,
+        "Passing": 0,
+        "Failing": 0,
+        "Inconclusive": 3
+      },
+      {
+        "Folder": "percentagePostsWithoutComments.feature",
+        "Total": 4,
+        "Passing": 0,
+        "Failing": 0,
+        "Inconclusive": 4
       }
     ],
     "NotTestedFolders": [
       {
-        "Folder": "percentagePostsWithoutComments.feature",
+        "Folder": "delete.feature",
         "Total": 0,
         "Passing": 0,
         "Failing": 0,
         "Inconclusive": 0
       },
       {
-        "Folder": "delete.feature",
+        "Folder": "topThreeMembers.feature",
+        "Total": 0,
+        "Passing": 0,
+        "Failing": 0,
+        "Inconclusive": 0
+      },
+      {
+        "Folder": "percentagePostsWithoutComments.feature",
         "Total": 0,
         "Passing": 0,
         "Failing": 0,
@@ -294,35 +430,42 @@ jsonPWrapper ({
       }
     ],
     "Scenarios": {
-      "Total": 6,
+      "Total": 9,
       "Passing": 0,
       "Failing": 0,
-      "Inconclusive": 6
+      "Inconclusive": 9
     },
     "Features": {
-      "Total": 2,
+      "Total": 3,
       "Passing": 0,
       "Failing": 0,
-      "Inconclusive": 2
+      "Inconclusive": 3
     },
     "FoldersWithTestKinds": [
-      {
-        "Folder": "percentagePostsWithoutComments.feature",
-        "Total": 4,
-        "Automated": 4,
-        "Manual": 0,
-        "NotTested": 0
-      },
       {
         "Folder": "delete.feature",
         "Total": 2,
         "Automated": 2,
         "Manual": 0,
         "NotTested": 0
+      },
+      {
+        "Folder": "topThreeMembers.feature",
+        "Total": 3,
+        "Automated": 3,
+        "Manual": 0,
+        "NotTested": 0
+      },
+      {
+        "Folder": "percentagePostsWithoutComments.feature",
+        "Total": 4,
+        "Automated": 4,
+        "Manual": 0,
+        "NotTested": 0
       }
     ]
   },
   "Configuration": {
-    "GeneratedOn": "17 November 2023 13:45:01"
+    "GeneratedOn": "17 November 2023 18:20:55"
   }
 });
